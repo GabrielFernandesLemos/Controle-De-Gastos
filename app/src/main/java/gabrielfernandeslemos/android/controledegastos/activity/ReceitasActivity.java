caa545a51d3d819145009ba7a5faaa873be4dc54
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2019-12-11 12:18:56 -0300

    Implement error treatment and save data

--- app/src/main/java/gabrielfernandeslemos/android/controledegastos/activity/ReceitasActivity.java
+++ app/src/main/java/gabrielfernandeslemos/android/controledegastos/activity/ReceitasActivity.java
@@ -7,6 +7,7 @@ import gabrielfernandeslemos.android.controledegastos.model.Movimentação;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -14,6 +15,8 @@ public class ReceitasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    Double despesaGerada;
+    Double despesaTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +33,50 @@ public class ReceitasActivity extends AppCompatActivity {
     }
 
     public void salvarDespesa(){
-        Movimentação movimentação = new Movimentação();
-        movimentação.setValor(Double.parseDouble(campoValor.getText().toString()));
-        movimentação.setCategoria(campoCategoria.getText().toString());
-        movimentação.setData(campoData.getText().toString());
-        movimentação.setTipo("d");
+
+        if (validarCamposDespesa()){
+            Movimentação movimentação = new Movimentação();
+
+            Double valorRecuperado = Double.parseDouble(campoValor.getText().toString());
+            movimentação.setValor(valorRecuperado);
+            movimentação.setCategoria(campoCategoria.getText().toString());
+            movimentação.setDescricao(campoDescricao.getText().toString());
+            movimentação.setData(campoData.getText().toString());
+            movimentação.setTipo("d");
+
+            despesaGerada = valorRecuperado;
+            despesaTotal = despesaTotal + despesaGerada;
+        }
+
+    }
+
+    public Boolean validarCamposDespesa(){
+
+        String valor = campoValor.getText().toString();
+        String categoria = campoCategoria.getText().toString();
+        String descricao = campoDescricao.getText().toString();
+        String data = campoData.getText().toString();
+
+        if ( !valor.isEmpty() ){
+            if ( !categoria.isEmpty() ){
+                if ( !descricao.isEmpty() ){
+                    if ( !data.isEmpty() ){
+                        return true;
+                    }else{
+                        Toast.makeText(ReceitasActivity.this, "Data não foi preenchido!", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }else{
+                    Toast.makeText(ReceitasActivity.this, "Descrição não foi preenchido!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }else{
+                Toast.makeText(ReceitasActivity.this, "Categoria não foi preenchido!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }else{
+            Toast.makeText(ReceitasActivity.this, "Valor não foi preenchido!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
